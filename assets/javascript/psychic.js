bcc935680843c7e5a40611eee3706b3350d3a61c
Heather Pauzer <heather.pauzer@gmail.com>
2017-08-12 01:17:01 -0500

    upload.4

--- assets/javascript/psychic.js
+++ assets/javascript/psychic.js
@@ -1,33 +1,48 @@
 // Array of the options.
-var alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 // This function is run whenever the user presses a key.
 var losses = 0;
 var wins = 0;
-var guesses = 10;
+var guesses = 26;
 
-var computerChoices = alphabet[Math.floor(Math.random() * alphabet.length)];
 
-console.log(computerChoices);
-
-document.onkeypress = function (event) {
+document.onkeyup = function (event) {
     var userGuess = event.key;
-    console.log(userGuess);
+    guesses = (guesses - 1);
+
+    var computerChoices = alphabet[Math.floor(Math.random() * alphabet.length)];
+
+    console.log("User guess: " + userGuess);
+    console.log("Computer's choice: " + computerChoices);
+    console.log("Guesses left: " + guesses);
+    console.log("Player's wins: " + wins);
+
 
     if (userGuess === computerChoices) {
-        wins++;
-        alert("You won." + "You have "
-            guesses + " left.");
+        wins = (wins + 1);
+        return (wins);
+
         //Alerts are supposed to show up in boxes rather than window.
     } else {
-        guesses--;
-        alert("You have " + (guesses) + " left.");
+        losses = (losses + 1);
+        return (losses);
+        console.log("Player's losses: " + losses);
+        //alert("You have " + (guesses) + " left.");
     }
 
     if (guesses === 0) {
-        losses++;
-        alert("No more guesses left.  Game Over.");
+        return ("game over");
+
+        //alert("No more guesses left.  Game Over.");
+    } else {
+        alert("Take another guess.")
+
     }
+
+
+
+
 
     document.getElementById.innerHTML = wins;
 
